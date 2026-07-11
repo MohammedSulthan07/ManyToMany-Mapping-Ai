5f3c1b9b7dd79a40069129a792f47dd9269fec65
Mohammed Sulthan <msulthan139@gmail.com>
2026-07-11 12:55:16 +0530

    Added Student-ManyToMany-Task Fixed

--- Student-Many-To-Many-Task/src/main/java/com/mapping/hibernate/entity/Student.java
+++ Student-Many-To-Many-Task/src/main/java/com/mapping/hibernate/entity/Student.java
@@ -39,10 +39,10 @@ public class Student {
 
 
     @ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE},fetch = FetchType.EAGER)
-    @JoinTable(name="student_course_table",
-            joinColumns = @JoinColumn(name="course_id",
-                    referencedColumnName = "id",nullable=false),
-            inverseJoinColumns = @JoinColumn(name="course_id",referencedColumnName = "id",nullable=false))
+    @JoinTable(
+            name = "student_course",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
     private Set<Course> courseList = new HashSet<>();
 
 }
